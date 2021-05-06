8d55064d9ef67392b9ef1610b66843548d4b9c93
Giuliano Serawop <gserawop13@gmail.com>
2021-05-05 21:13:15 -0600

    make a file structure and added some starter code in components

---
+++ sandspowerwashingandlawncare/src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function footer() {
+  return (
+    <footer style={} >
+      <h1 className="text-center">
+          
+      </h1>
+    </footer>
+  );
+}
+
+export default footer;
